3fea69e95846e2f80257a3056645a2b8779826e7
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-06-15 11:57:08 +0300

    the template is ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { observer } from 'mobx-react'
 
-function App() {
+@observer
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>This template was created in 15.06.20 11:54</p>
+  )}
 }
 
 export default App;
