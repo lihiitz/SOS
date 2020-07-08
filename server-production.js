304583b765d507391bc5a557a6ed8f14d9230d81
Veronika Adelson <adelson1606@gmail.com>
2020-07-08 09:56:01 +0300

    prepare env

--- server-production.js
+++ server-production.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 
-const { app } = require('./server')
+const { app } = require('./server-common')
 
 app.use(express.static(path.join(__dirname, 'build')));
 
